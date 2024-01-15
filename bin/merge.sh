ceee3e8ab1e175e2b0a3de2cc19c09a147a7df91
Kristine Dosvik <krdosvik@silabs.com>
2024-01-15 10:52:13 +0100

    Remove no-cmd-output option
    Signed-off-by: Kristine Dosvik <krdosvik@silabs.com>

--- bin/merge.sh
+++ bin/merge.sh
@@ -18,22 +18,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# print command output or not:
-CMD_OUTPUT=$2
-
-if [[ $CMD_OUTPUT == "--no-cmd-output" ]]; then
-  # 'tty' represents 'current console'
-  # changing 'tty' to 'null' disables output
-  OUTPUT_DEV="/dev/null"
-else
-  OUTPUT_DEV="/dev/tty"
-fi
-echo $OUTPUT_DEV
-
-{
-  echo hi
-  git status
-} 2>&1 > $OUTPUT_DEV
 
 #Variables:
 date_time=$(date +%Y.%m.%d-%H.%M)
@@ -41,12 +25,11 @@ date_time=$(date +%Y.%m.%d-%H.%M)
 
 usage() {
 
-  echo "usage: $0 --[s_into_x-dv|sdev_into_xdev|xdev_into_sdev|rejection-diff] --<no-cmd-output>"
+  echo "usage: $0 --[s_into_x-dv|sdev_into_xdev|xdev_into_sdev|rejection-diff]"
   echo "--s_into_x-dv     Do a merge of core-v-verif cv32e40s/dev cv32e40s directory into cv32e40x-dv main (make sure the clonetb script has run)"
   echo "--sdev_into_xdev  Do a merge of core-v-verif cv32e40s/dev into core-v-verif cv32e40x/dev"
   echo "--xdev_into_sdev  Do a merge of core-v-verif cv32e40x/dev into core-v-verif cv32e40s/dev"
   echo "--rejection-diff  Merge s/dev to x-dv, using 'theirs'"
-  echo "--no-cmd-output   Do not print cmd output. If the output is not as expected it can hide important error messages."
 
   exit 1
 
@@ -71,24 +54,21 @@ merge_cv32e40s_into_cv32e40x-dv () {
   cd cv32e40x
 
   echo "=== Make a branch in cv32e40x-dv that contain core-v-verif's cv32e40s folder from the cv32e40s/dev branch ==="
-  {
-    git remote add ohw_cvv git@github.com:openhwgroup/core-v-verif.git
-    git fetch ohw_cvv
-    git checkout -b cvv_$date_time ohw_cvv/cv32e40s/dev
-    git subtree split --prefix cv32e40s -b cv32e40s_$date_time
-  } 2>&1 > $OUTPUT_DEV
+  git remote add ohw_cvv git@github.com:openhwgroup/core-v-verif.git
+  git fetch ohw_cvv
+  git checkout -b cvv_$date_time ohw_cvv/cv32e40s/dev
+  git subtree split --prefix cv32e40s -b cv32e40s_$date_time
+
 
   echo "=== Make a branch based on the latest cv32e40x-dv content ==="
-  {
-    git remote add ohw_x-dv git@github.com:openhwgroup/cv32e40x-dv.git
-    git fetch ohw_x-dv
-    git checkout -b merge_cv32e40s_$date_time ohw_x-dv/main
-  } 2>&1 > $OUTPUT_DEV
+  git remote add ohw_x-dv git@github.com:openhwgroup/cv32e40x-dv.git
+  git fetch ohw_x-dv
+  git checkout -b merge_cv32e40s_$date_time ohw_x-dv/main
+
 
   echo "=== Merge ==="
-  {
-    git merge -X find-renames --no-ff --no-commit cv32e40s_$date_time
-  } 2>&1 > $OUTPUT_DEV
+  git merge -X find-renames --no-ff --no-commit cv32e40s_$date_time
+
 }
 
 
@@ -106,12 +86,11 @@ move_files_40s_into_40x () {
 
     *)
     echo "=== Rename files ==="
-      {
-        find . -type d | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | xargs -n1 dirname | awk '{gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 mkdir -p
-        find . -type d | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | awk '{printf $1; printf " "; gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 git mv -f
-        find . -type f | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | xargs -n1 dirname | awk '{gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 mkdir -p
-        find . -type f | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | awk '{printf $1; printf " "; gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 git mv -f
-      } 2>&1 > $OUTPUT_DEV
+      find . -type d | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | xargs -n1 dirname | awk '{gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 mkdir -p
+      find . -type d | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | awk '{printf $1; printf " "; gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 git mv -f
+      find . -type f | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | xargs -n1 dirname | awk '{gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 mkdir -p
+      find . -type f | egrep -iv '\/\.|40sx|40xs' | grep -i 40s | awk '{printf $1; printf " "; gsub(/40s/, "40x"); gsub(/40S/, "40X"); print}' | xargs -n2 git mv -f
+
       ;;
   esac
 }
@@ -125,10 +104,10 @@ substitute_file_content_40s_into_40x () {
   case $yn in
     [Yy])
       echo "=== Content substitution ===";
-      {
-        find . -type f -exec grep -Il . {} + | egrep -iv '\/\.|40sx|40xs' | xargs -n1 sed -i 's/40s/40x/g'
-        find . -type f -exec grep -Il . {} + | egrep -iv '\/\.|40sx|40xs' | xargs -n1 sed -i 's/40S/40X/g'
-      } 2>&1 > $OUTPUT_DEV
+
+      find . -type f -exec grep -Il . {} + | egrep -iv '\/\.|40sx|40xs' | xargs -n1 sed -i 's/40s/40x/g'
+      find . -type f -exec grep -Il . {} + | egrep -iv '\/\.|40sx|40xs' | xargs -n1 sed -i 's/40S/40X/g'
+
       return
       ;;
 
@@ -145,19 +124,16 @@ merge_sdev_into_xdev () {
   echo $'\n======= Merge of core-v-verif cv32e40s/dev into cv32e40x/dev =======\n'
 
   echo "=== Make a core-v-verif/cv32e40s/dev branch ==="
-  {
-    git checkout -b cvv_sdev_$date_time ohw_cvv/cv32e40s/dev
-  } 2>&1 > $OUTPUT_DEV
+  git checkout -b cvv_sdev_$date_time ohw_cvv/cv32e40s/dev
+
 
   echo "=== Make a core-v-verif/cv32e40x/dev branch ==="
-  {
-    git checkout -b cvv_xdev_$date_time ohw_cvv/cv32e40x/dev
-  } 2>&1 > $OUTPUT_DEV
+  git checkout -b cvv_xdev_$date_time ohw_cvv/cv32e40x/dev
+
 
   echo "=== Merge ==="
-  {
-    git merge --no-commit --no-ff cvv_sdev_$date_time
-  } 2>&1 > $OUTPUT_DEV
+  git merge --no-commit --no-ff cvv_sdev_$date_time
+
 
 }
 
@@ -167,19 +143,16 @@ merge_xdev_into_sdev () {
   echo $'\n======= Merge of core-v-verif cv32e40x/dev into cv32e40s/dev =======\n'
 
   echo "=== Make a core-v-verif/cv32e40s/dev branch ==="
-  {
-    git checkout -b cvv_xdev_$date_time ohw_cvv/cv32e40x/dev
-  } 2>&1 > $OUTPUT_DEV
+  git checkout -b cvv_xdev_$date_time ohw_cvv/cv32e40x/dev
+
 
   echo "=== Make a core-v-verif/cv32e40s/dev branch ==="
-  {
-    git checkout -b cvv_sdev_$date_time ohw_cvv/cv32e40s/dev
-  } 2>&1 > $OUTPUT_DEV
+  git checkout -b cvv_sdev_$date_time ohw_cvv/cv32e40s/dev
+
 
   echo "=== Merge ==="
-  {
-    git merge --no-commit --no-ff cvv_xdev_$date_time
-  } 2>&1 > $OUTPUT_DEV
+  git merge --no-commit --no-ff cvv_xdev_$date_time
+
 
 }
 
@@ -191,9 +164,8 @@ clone_x_dv() {
   read -p "This overwrites 'cv32e40x/'. Continue? (default: y, n) " yn
   continue_check $yn
 
-  {
-    ./bin/clonetb --x-main
-  } 2>&1 > $OUTPUT_DEV
+  ./bin/clonetb --x-main
+
 
 }
 
@@ -275,11 +247,9 @@ need_40s_40x-dv_merge(){
 get_branch_cv32e40s_dev() {
 
   echo -e "\n== Enter an updated cv32e40s/dev branch =="
-  {
-    git remote add ohw_cvv git@github.com:openhwgroup/core-v-verif.git
-    git fetch ohw_cvv
-    git checkout ohw_cvv/cv32e40s/dev
-  } 2>&1 > $OUTPUT_DEV
+  git remote add ohw_cvv git@github.com:openhwgroup/core-v-verif.git
+  git fetch ohw_cvv
+  git checkout ohw_cvv/cv32e40s/dev
 
 }
 
